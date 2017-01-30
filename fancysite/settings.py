283fd9b1544f064ff261fae06bc6c7908beec1d4
Adam Campbell <atcampbell@Adams-MBP.lan>
2017-01-30 22:21:06 +0000

    Turned debug off

--- fancysite/settings.py
+++ fancysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '6z)w#l4gstgwf)$25dpste43$k!9+88remz(u)$wb_y$_=2qq0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
